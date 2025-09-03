8fbb14164e66f8d6b35c173132e3ffcef10f91a4
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-09-03 10:12:56 +0300

    Update mainb.py

--- mainb.py
+++ mainb.py
@@ -11,7 +11,7 @@ from datetime import datetime
 from os import environ
 from re import match
 from gpytranslate import Translator
-from nextcord import Intents, Client, Game
+from nextcord import Intents, Client, Game, app_commands
 from aiohttp import ClientSession
 from orjson import loads
 

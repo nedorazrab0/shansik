37b6bd9ba946f8ded0d1c5941cd70da9aa00d54f
valenok <141725500+nedorazrab0@users.noreply.github.com>
2025-09-03 21:30:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -141,8 +141,12 @@ async def timestamp(
 async def timezone(
     ctx: Interaction,
     hour: int,
-    source_zone: int = SlashOption(choices=list(range(-24, 25))),
-    target_zone: int = SlashOption(choices=list(range(-24, 25)))
+    source_zone: int = SlashOption(choices=[-12, -11, -10, -9, -8, -7, -6, -5,
+                                            -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,
+                                            6, 7, 8, 9, 10, 11, 12]),
+    target_zone: int = SlashOption(choices=[-12, -11, -10, -9, -8, -7, -6, -5,
+                                            -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,
+                                            6, 7, 8, 9, 10, 11, 12])
 ):
     converted = hour + source_zone + target_zone
     if converted > 24:
